3d2bb92bbcc477ecc4a7ceddce9bc529e9782e1f
Jangseungu <23_11118@daejin.sen.hs.kr>
2025-01-08 11:22:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import streamlit as st
-
+import random
 st.title("'나의 첫번째 앱")
 st.tast('/n/n')
 st.write('안녕하세요, 저는 🥩😊입니다.
 st.write('저의 이메일 주소: 23_11118daejin.sen.hs.kr
 
 st.button("Reset", type="primary")
-if st.button("Say hello"):
-    st.write("Why hello there")
+if st.button("난수 생성"):
+    st.write("random.randint(1,1000"))
 else:
     st.write("Goodbye")
 
